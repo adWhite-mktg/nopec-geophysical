5365bb83c255581c7c3b13bf799020e1f38c403d
Juan Olvera <1130549+j0lv3r4@users.noreply.github.com>
2019-07-15 16:26:33 -0500

    Update social-icons.php

--- inc/social-icons.php
+++ inc/social-icons.php
@@ -1,8 +1,8 @@
 <div class="social-icons bg-brand-green">
     <ul class="list-unstyled bg-brand-white-transparent mb-0 social-list">
-        <li class="social-item"><a class="social-link" href="#"><i class="fab fa-twitter"></i></a></li>
-        <li class="social-item"><a class="social-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
-        <li class="social-item"><a class="social-link" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-        <li class="social-item"><a class="social-link" href="#"><i class="fab fa-instagram"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://twitter.com/TGScompany" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://www.facebook.com/TGSgeosciencedatacompany" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://www.linkedin.com/company/tgs_220114/?trk=cp_followed_name_tgs_220114" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://www.instagram.com/tgsgeo/" target="_blank"><i class="fab fa-instagram"></i></a></li>
     </ul>
 </div> <!-- .jumbotron-social-icons -->

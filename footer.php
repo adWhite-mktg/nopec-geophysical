f2b7c1d1991a9047a2dafc4600926c1c874a4e52
Juan Olvera <1130549+j0lv3r4@users.noreply.github.com>
2019-07-15 16:27:24 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -18,10 +18,10 @@
 	<footer id="colophon" class="site-footer px-4 bg-brand-green text-light text-center">
 		<div class="container-fluid d-flex flex-column">
             <ul class="list-unstyled mx-auto social-list">
-				<li class="social-item"><a class="social-link" href="#"><i class="fab fa-twitter"></i></a></li>
-				<li class="social-item"><a class="social-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
-				<li class="social-item"><a class="social-link" href="#"><i class="fab fa-linkedin-in"></i></a></li>
-				<li class="social-item"><a class="social-link" href="#"><i class="fab fa-instagram"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://twitter.com/TGScompany" target="_blank"><i class="fab fa-twitter"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://www.facebook.com/TGSgeosciencedatacompany" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://www.linkedin.com/company/tgs_220114/?trk=cp_followed_name_tgs_220114" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+        <li class="social-item"><a class="social-link" href="https://www.instagram.com/tgsgeo/" target="_blank"><i class="fab fa-instagram"></i></a></li>
 			</ul>
 			<p class="mb-0 w-100 lead font-weight-normal">&copy; 2019 NOPEC Geophysical</p>
 		</div> <!-- .container -->
